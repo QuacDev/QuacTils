b39c92ae58f2d5fa3bcd6af8f85b740f01b1f03d
QuacGH <vdwal.joeri@gmail.com>
2021-09-20 17:05:44 +0200

    Remove

--- build/sources/main/java/com/quac/sbmod/EventHandlers/TestEvents.java
+++
@@ -1,8 +0,0 @@
-package com.quac.sbmod.EventHandlers;
-
-import net.minecraftforge.client.event.DrawBlockHighlightEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
-public class TestEvents {
-
-}
